07e7f5f4634eab2ff4f6090479b4a287da1e686e
MeShootIn <dmitriimishutin@gmail.com>
2023-01-07 22:17:02 +0500

    feat(src): add source code

---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "./sum.h"
+
+int main (int argc, char *argv[])
+{
+  printf("sum(2, 3) = %d", sum(2, 3));
+
+  return 0;
+}
---
+++ src/sum.c
@@ -0,0 +1,8 @@
+#ifndef SUM_C
+#define SUM_C
+
+int sum(int a, int b) {
+  return a + b;
+}
+
+#endif // !SUM_C
